fb95400596dabece573c02c1db836d2ae72c70d7
tanvishinde03 <shindetanvi0203@gmail.com>
2023-02-10 23:22:34 +0530

    [Tanvi] Add UC1 - Length of Array

---
+++ ArrayProblems/ArrayProblem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayProblems
+{
+    public class ArrayProblem
+    {
+        public void CheckLenghtOfArray()
+        {
+            int[] array = { 2, 4, 5, 6, 7, 8, 9, 1, 3 };
+            int arrayLength = array.Length;
+
+            Console.WriteLine(" The Length of the array:" + arrayLength);
+            Console.ReadLine();
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ ArrayProblems/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayProblems
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1 ArrayLength");
+            Console.WriteLine("choose the option");
+            int opiton = Convert.ToInt32(Console.ReadLine());
+
+            switch (opiton)
+            {
+                case 1:
+                    ArrayProblem arrayLength = new ArrayProblem();
+                    arrayLength.CheckLenghtOfArray();
+                    break;
+
+
+            }
+        }
+    }
+}
